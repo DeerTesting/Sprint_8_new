0c4c6f37832832dd338d8c847c816ce7d8b4984e
Iuliia Mihaylovskaia <DeeratheDear@yandex.ru>
2024-06-09 17:39:32 +0300

    second commit

--- src/test/java/NegativeTest.java
+++ src/test/java/NegativeTest.java
@@ -1,15 +1,12 @@
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
-import io.restassured.RestAssured;
 import org.junit.Assert;
-import org.junit.Before;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(Parameterized.class)
 public class NegativeTest {
@@ -33,8 +30,15 @@ public class NegativeTest {
     }
 
     @Test
+    @DisplayName("Test negative scenarios")
+    @Description("This test is for check that name cant be printed")
     public void NamePrintTest(){
         Account account = new Account(name);
+        nameCheck(account);
+    }
+
+    @Step
+    public void nameCheck(Account account){
         Assert.assertFalse(account.checkNameToEmboss());
     }
 
--- src/test/java/PositiveTest.java
+++ src/test/java/PositiveTest.java
@@ -1,15 +1,11 @@
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
-import io.restassured.RestAssured;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(Parameterized.class)
 public class PositiveTest {
@@ -30,8 +26,15 @@ public class PositiveTest {
     }
 
     @Test
-    public void NamePrintTest(){
+    @DisplayName("Test positive scenarios")
+    @Description("This test is for check that name can be printed")
+    public void namePrintTest(){
         Account account = new Account(name);
+        nameCheck(account);
+    }
+
+    @Step
+    public void nameCheck(Account account){
         Assert.assertTrue(account.checkNameToEmboss());
     }
 
